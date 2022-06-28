6b8525d33f4dc78ad6133aacb11663fb332b2533
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2022-06-28 20:31:24 +0300

    CalculatorDX

---
+++ CalculatorDX/CalculatorDX/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace CalculatorDX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton5 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton6 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton7 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton8 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton9 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton10 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton11 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton12 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton13 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton14 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton15 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton16 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton17 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton18 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton19 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton20 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton21 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton22 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Primary;
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Location = new System.Drawing.Point(257, 465);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "=";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(181, 465);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = ".";
+            this.simpleButton2.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(105, 465);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton3.TabIndex = 2;
+            this.simpleButton3.Text = "0";
+            this.simpleButton3.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Location = new System.Drawing.Point(29, 465);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton4.TabIndex = 3;
+            this.simpleButton4.Text = "+/-";
+            // 
+            // simpleButton5
+            // 
+            this.simpleButton5.Location = new System.Drawing.Point(29, 389);
+            this.simpleButton5.Name = "simpleButton5";
+            this.simpleButton5.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton5.TabIndex = 7;
+            this.simpleButton5.Text = "1";
+            this.simpleButton5.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton6
+            // 
+            this.simpleButton6.Location = new System.Drawing.Point(105, 389);
+            this.simpleButton6.Name = "simpleButton6";
+            this.simpleButton6.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton6.TabIndex = 6;
+            this.simpleButton6.Text = "2";
+            this.simpleButton6.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton7
+            // 
+            this.simpleButton7.Location = new System.Drawing.Point(181, 389);
+            this.simpleButton7.Name = "simpleButton7";
+            this.simpleButton7.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton7.TabIndex = 5;
+            this.simpleButton7.Text = "3";
+            this.simpleButton7.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton8
+            // 
+            this.simpleButton8.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Warning;
+            this.simpleButton8.Appearance.Options.UseBackColor = true;
+            this.simpleButton8.Location = new System.Drawing.Point(257, 389);
+            this.simpleButton8.Name = "simpleButton8";
+            this.simpleButton8.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton8.TabIndex = 4;
+            this.simpleButton8.Text = "+";
+            this.simpleButton8.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton9
+            // 
+            this.simpleButton9.Location = new System.Drawing.Point(29, 313);
+            this.simpleButton9.Name = "simpleButton9";
+            this.simpleButton9.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton9.TabIndex = 11;
+            this.simpleButton9.Text = "4";
+            this.simpleButton9.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton10
+            // 
+            this.simpleButton10.Location = new System.Drawing.Point(105, 313);
+            this.simpleButton10.Name = "simpleButton10";
+            this.simpleButton10.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton10.TabIndex = 10;
+            this.simpleButton10.Text = "5";
+            this.simpleButton10.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton11
+            // 
+            this.simpleButton11.Location = new System.Drawing.Point(181, 313);
+            this.simpleButton11.Name = "simpleButton11";
+            this.simpleButton11.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton11.TabIndex = 9;
+            this.simpleButton11.Text = "6";
+            this.simpleButton11.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton12
+            // 
+            this.simpleButton12.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Warning;
+            this.simpleButton12.Appearance.Options.UseBackColor = true;
+            this.simpleButton12.Location = new System.Drawing.Point(257, 313);
+            this.simpleButton12.Name = "simpleButton12";
+            this.simpleButton12.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton12.TabIndex = 8;
+            this.simpleButton12.Text = "-";
+            this.simpleButton12.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton13
+            // 
+            this.simpleButton13.Location = new System.Drawing.Point(29, 237);
+            this.simpleButton13.Name = "simpleButton13";
+            this.simpleButton13.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton13.TabIndex = 15;
+            this.simpleButton13.Text = "7";
+            this.simpleButton13.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton14
+            // 
+            this.simpleButton14.Location = new System.Drawing.Point(105, 237);
+            this.simpleButton14.Name = "simpleButton14";
+            this.simpleButton14.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton14.TabIndex = 14;
+            this.simpleButton14.Text = "8";
+            this.simpleButton14.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton15
+            // 
+            this.simpleButton15.Location = new System.Drawing.Point(181, 237);
+            this.simpleButton15.Name = "simpleButton15";
+            this.simpleButton15.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton15.TabIndex = 13;
+            this.simpleButton15.Text = "9";
+            this.simpleButton15.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton16
+            // 
+            this.simpleButton16.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Warning;
+            this.simpleButton16.Appearance.Options.UseBackColor = true;
+            this.simpleButton16.Location = new System.Drawing.Point(257, 237);
+            this.simpleButton16.Name = "simpleButton16";
+            this.simpleButton16.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton16.TabIndex = 12;
+            this.simpleButton16.Text = "*";
+            this.simpleButton16.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton17
+            // 
+            this.simpleButton17.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
+            this.simpleButton17.Appearance.Options.UseBackColor = true;
+            this.simpleButton17.Location = new System.Drawing.Point(29, 161);
+            this.simpleButton17.Name = "simpleButton17";
+            this.simpleButton17.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton17.TabIndex = 19;
+            this.simpleButton17.Text = "%";
+            this.simpleButton17.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton18
+            // 
+            this.simpleButton18.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
+            this.simpleButton18.Appearance.Options.UseBackColor = true;
+            this.simpleButton18.Location = new System.Drawing.Point(105, 161);
+            this.simpleButton18.Name = "simpleButton18";
+            this.simpleButton18.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton18.TabIndex = 18;
+            this.simpleButton18.Text = "CE";
+            this.simpleButton18.Click += new System.EventHandler(this.simpleButton18_Click);
+            // 
+            // simpleButton19
+            // 
+            this.simpleButton19.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
+            this.simpleButton19.Appearance.Options.UseBackColor = true;
+            this.simpleButton19.Location = new System.Drawing.Point(181, 161);
+            this.simpleButton19.Name = "simpleButton19";
+            this.simpleButton19.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton19.TabIndex = 17;
+            this.simpleButton19.Text = "X";
+            this.simpleButton19.Click += new System.EventHandler(this.sb_removeone);
+            // 
+            // simpleButton20
+            // 
+            this.simpleButton20.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Warning;
+            this.simpleButton20.Appearance.Options.UseBackColor = true;
+            this.simpleButton20.Location = new System.Drawing.Point(257, 161);
+            this.simpleButton20.Name = "simpleButton20";
+            this.simpleButton20.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton20.TabIndex = 16;
+            this.simpleButton20.Text = "/";
+            this.simpleButton20.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.EditValue = "";
+            this.textEdit1.Location = new System.Drawing.Point(29, 23);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(73)))), ((int)(((byte)(88)))));
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 30F, System.Drawing.FontStyle.Bold);
+            this.textEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.textEdit1.Properties.ReadOnly = true;
+            this.textEdit1.Size = new System.Drawing.Size(298, 54);
+            this.textEdit1.TabIndex = 20;
+            // 
+            // simpleButton21
+            // 
+            this.simpleButton21.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Success;
+            this.simpleButton21.Appearance.Options.UseBackColor = true;
+            this.simpleButton21.Location = new System.Drawing.Point(181, 85);
+            this.simpleButton21.Name = "simpleButton21";
+            this.simpleButton21.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton21.TabIndex = 24;
+            this.simpleButton21.Text = "(";
+            this.simpleButton21.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // simpleButton22
+            // 
+            this.simpleButton22.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Success;
+            this.simpleButton22.Appearance.Options.UseBackColor = true;
+            this.simpleButton22.Location = new System.Drawing.Point(257, 85);
+            this.simpleButton22.Name = "simpleButton22";
+            this.simpleButton22.Size = new System.Drawing.Size(70, 70);
+            this.simpleButton22.TabIndex = 23;
+            this.simpleButton22.Text = ")";
+            this.simpleButton22.Click += new System.EventHandler(this.sb_buttonadd);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(351, 552);
+            this.Controls.Add(this.simpleButton21);
+            this.Controls.Add(this.simpleButton22);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton17);
+            this.Controls.Add(this.simpleButton18);
+            this.Controls.Add(this.simpleButton19);
+            this.Controls.Add(this.simpleButton20);
+            this.Controls.Add(this.simpleButton13);
+            this.Controls.Add(this.simpleButton14);
+            this.Controls.Add(this.simpleButton15);
+            this.Controls.Add(this.simpleButton16);
+            this.Controls.Add(this.simpleButton9);
+            this.Controls.Add(this.simpleButton10);
+            this.Controls.Add(this.simpleButton11);
+            this.Controls.Add(this.simpleButton12);
+            this.Controls.Add(this.simpleButton5);
+            this.Controls.Add(this.simpleButton6);
+            this.Controls.Add(this.simpleButton7);
+            this.Controls.Add(this.simpleButton8);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.Click += new System.EventHandler(this.sb_buttonadd);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton5;
+        private DevExpress.XtraEditors.SimpleButton simpleButton6;
+        private DevExpress.XtraEditors.SimpleButton simpleButton7;
+        private DevExpress.XtraEditors.SimpleButton simpleButton8;
+        private DevExpress.XtraEditors.SimpleButton simpleButton9;
+        private DevExpress.XtraEditors.SimpleButton simpleButton10;
+        private DevExpress.XtraEditors.SimpleButton simpleButton11;
+        private DevExpress.XtraEditors.SimpleButton simpleButton12;
+        private DevExpress.XtraEditors.SimpleButton simpleButton13;
+        private DevExpress.XtraEditors.SimpleButton simpleButton14;
+        private DevExpress.XtraEditors.SimpleButton simpleButton15;
+        private DevExpress.XtraEditors.SimpleButton simpleButton16;
+        private DevExpress.XtraEditors.SimpleButton simpleButton17;
+        private DevExpress.XtraEditors.SimpleButton simpleButton18;
+        private DevExpress.XtraEditors.SimpleButton simpleButton19;
+        private DevExpress.XtraEditors.SimpleButton simpleButton20;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton21;
+        private DevExpress.XtraEditors.SimpleButton simpleButton22;
+    }
+}
+
---
+++ CalculatorDX/CalculatorDX/Form1.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorDX
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sb_buttonadd(object sender, EventArgs e)
+        {
+            try
+            {
+                textEdit1.Text += ((SimpleButton)sender).Text;
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void simpleButton18_Click(object sender, EventArgs e)
+        {
+            textEdit1.Text = "";
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textEdit1.Text = Eval(textEdit1.Text).ToString();
+            }
+            catch (Exception)
+            {
+
+                textEdit1.Text = "";
+            }
+        }
+        public Double Eval(String expression)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            return Convert.ToDouble(table.Compute(expression, String.Empty));
+        }
+
+        private void sb_removeone(object sender, EventArgs e)
+        {
+            try
+            {
+                textEdit1.Text = textEdit1.Text.Substring(0, textEdit1.Text.Length - 1);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
